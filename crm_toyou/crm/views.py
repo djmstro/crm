f693fa7d64a0e3b5fb0df10024393ec4793c2843
Alexander Khlynov <djmstro@mail.ru>
2022-08-05 05:15:43 +0300

    Вывод проектов

--- crm_toyou/crm/views.py
+++ crm_toyou/crm/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Project
+
+
+class HomeProjects(ListView):
+    model = Project
+    template_name = 'crm/index.html'
+    context_object_name = 'projects'
+
 
 def index(request):
     return render(request, 'crm/index.html')
-
